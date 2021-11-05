dda384fe3db15bd3018560854477b22f89a94582
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2021-11-05 07:01:14 +0100

    first commit
    this commit has the bgColor change with VUE by changing the data manually, yet to include a function to drive it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+new Vue ({
+    el: '#vue-app',
+    data: {
+            SH1: true,
+            SH2: false,
+            SH3: false,
+            SH4: false
 
+                       
+    },
+
+    methods: {
+        
+     
+        
+
+    },
+
+    computed: {
+
+     backChange: function(){
+         return{
+             bgcolor1: this.SH1,
+             bgcolor2: this.SH2,
+             bgcolor3: this.SH3,
+             bgcolor4: this.SH4
+         }
+     }
+
+
+    }
+
+});
